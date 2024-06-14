c0fb7c271a7864e6c67e2c977d8b3586937f4315
khushiram saini <prabhakarsaini800@gmail.com>
2024-06-14 19:53:20 +0530

    Project setup

---
+++ src/utils/constant.js
@@ -0,0 +1,4 @@
+export const CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/';
+
+export const LOGO_URL =
+  'https://img.freepik.com/free-vector/hand-drawn-spicy-logo-design_23-2149667267.jpg?w=740&t=st=1717958594~exp=1717959194~hmac=5ce062f266f9393ea9693765b5f8277bfc709547ebe7fb96a42918d9936e7ae5';
